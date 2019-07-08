2d79da2ea7e95ff9e0c93986d0f40c2aba3c268d
paulurbina <paulurbian@hotmail.com>
2019-07-08 16:08:05 -0500

    enlace method signin with #create-newUser-button

--- auth.js
+++ auth.js
@@ -1,5 +1,11 @@
 console.log('active');
 
+$('#create-newUser-button').click(function () {
+    var email = $('#inputIEmail').val();
+    var password = $('#inputPassword').val();
+    SignIn(email, password);
+});
+
 function SignIn(email, password) {
     firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
         // Handle Errors here.
