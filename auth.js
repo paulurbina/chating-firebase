0fab08b7ce394dfa0242ef62ec2e842cb799e9c2
paulurbina <paulurbian@hotmail.com>
2019-07-08 16:02:31 -0500

    create method signin

---
+++ auth.js
@@ -0,0 +1,10 @@
+console.log('active');
+
+function SignIn(email, password) {
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+      });
+}
