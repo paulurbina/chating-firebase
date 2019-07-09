71dfc0c753f4f1d9e1b3b38185cfeda1f6a4f283
usrcdimi <usrcdimi@cibertec.edu.pe>
2019-07-08 20:42:40 -0500

    method observed

--- auth.js
+++ auth.js
@@ -1,9 +1,13 @@
-console.log('active');
+//console.log('active');
+
+//variable global
+var currentUser = {};
 
 $('#create-newUser-button').click(function () {
     var email = $('#inputIEmail').val();
     var password = $('#inputPassword').val();
     SignIn(email, password);
+    console.log("New User:"+ " " + email + " " + password);
 });
 
 function SignIn(email, password) {
@@ -12,5 +16,26 @@ function SignIn(email, password) {
         var errorCode = error.code;
         var errorMessage = error.message;
         // ...
-      });
+        console.log(errorCode, errorMessage);
+      });      
 }
+
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      var displayName = user.displayName;
+      var email = user.email;
+      var emailVerified = user.emailVerified;
+      var photoURL = user.photoURL;
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+      var providerData = user.providerData;
+      currentUser = user;
+      console.log(currentUser.email + " has logged in");
+      
+      // ...
+    } else {
+      // User is signed out.
+      // ...
+    }
+  });
